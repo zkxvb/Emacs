e065ee8516a2634fe336a45302e5168844146d73
zkxvb <zkxvb@outlook.com>
2021-01-03 10:11:19 +0800

    first commit

---
+++ init.el
@@ -0,0 +1,125 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+
+  (when (>= emacs-major-version 24)
+    (require 'package)
+    (package-initialize)
+    (setq package-archives '(("gnu" . "http://mirrors.ustc.edu.cn/elpa/gnu/")
+                         ("melpa" . "http://mirrors.ustc.edu.cn/elpa/melpa/")
+                         ("melpa-stable" . "http://mirrors.ustc.edu.cn/elpa/melpa-stable/")
+                         ("org" . "http://mirrors.ustc.edu.cn/elpa/org/")))
+   ;; (add-to-list 'package-archives '("melpa", "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/") t)
+    )
+  (require 'cl)
+  ;; add whatever packages you want here
+  (defvar zkx/packages '(
+			 company
+			 monokai-theme
+			 hungry-delete
+			 swiper
+			 counsel
+			 smartparens
+			 js2-mode
+			 nodejs-repl
+			 exec-path-from-shell
+			 ) "Default packages")
+
+(setq package-selected-packages zkx/packages)
+
+  (defun zkx/packages-installed-p ()
+    (loop for pkg in zkx/packages
+      when (not (package-installed-p pkg)) do (return nil)
+      finally (return t)))
+  (unless (zkx/packages-installed-p )
+    (message "%s" "Refreshing package database...")
+    (package-refresh-contents)
+    (dolist (pkg zkx/packages)
+      (when (not (package-installed-p pkg))
+        (package-install pkg))))
+
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+;;(electric-indent-mode -1)
+(global-linum-mode t)
+(setq inhibit-splash-screen t)
+(setq-default cursor-type 'bar)
+(setq make-backup-files nil)
+
+(defun open-my-init-file()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+
+(global-set-key (kbd "<f2>") 'open-my-init-file)
+
+(global-company-mode t)
+
+(require 'org)
+(setq org-src-fontify-natively t)
+
+(setq org-agenda-files '("~/org"))
+(global-set-key (kbd "C-c a") 'org-agenda)
+
+(require 'recentf)
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+(global-set-key "\C-x\ \C-r" 'recentf-open-files)
+
+(delete-selection-mode t)
+
+(setq initial-frame-alist (quote ((fullscreen . maximized))))
+
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+
+(global-hl-line-mode t)
+(load-theme 'monokai t)
+
+(require 'hungry-delete)
+(global-hungry-delete-mode)
+
+(require 'smartparens-config)
+(smartparens-global-mode t)
+;;(add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
+;;(add-hook 'org-mode-hook 'smartparens-mode)
+
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "\C-c C-r") 'ivy-resume)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-h f") 'counsel-describe-function)
+(global-set-key (kbd "C-h v") 'counsel-describe-variable)
+
+;; config js2-mode for js files
+(setq auto-mode-alist
+      (append
+       '(("\\.js\\'" . js2-mode))
+       auto-mode-alist))
+
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+(global-set-key (kbd "C-h C-f") 'find-function)
+(global-set-key (kbd "C-h C-v") 'find-variable)
+(global-set-key (kbd "C-h C-k") 'find-function-on-key)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-idle-delay 0.08)
+ '(company-minimum-prefix-length 1)
+ '(custom-safe-themes
+   (quote
+    ("d9646b131c4aa37f01f909fbdd5a9099389518eb68f25277ed19ba99adeb7279" default))))
+ 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
